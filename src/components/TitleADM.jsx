3b6f4e76d9d87aefb6a2796fdc379c5362a46f6d
jorgezeza <jorgezeza2010@gmail.com>
2023-06-16 19:25:31 -0300

    Feat: isolate ADM title in a component

---
+++ src/components/TitleADM.jsx
@@ -0,0 +1,10 @@
+const TitleADM = ({ title }) => {
+  return (
+    <div className="content__header-adm">
+      <h1>{title}</h1>
+      <button className="btn lg" >Create new entry</button>
+    </div>
+  )
+}
+
+export default TitleADM
